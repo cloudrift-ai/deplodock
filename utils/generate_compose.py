df6c2b76a2db7d2fe72ea621ca4cd4dcbc9955ad
Dmitry Trifonov <slonegg@gmail.com>
2025-11-25 19:30:41 -0800

    remove CUDA_VISIBLE_DEVICES from vllm benchmark client at all

--- utils/generate_compose.py
+++ utils/generate_compose.py
@@ -123,19 +123,11 @@ def generate_benchmark_service(hf_directory: str, hf_token: str) -> str:
   benchmark:
     image: vllm/vllm-openai:latest
     container_name: vllm_benchmark_client
-    deploy:
-      resources:
-        reservations:
-          devices:
-            - driver: nvidia
-              count: all
-              capabilities: [gpu]
     volumes:
       - {hf_directory}:{hf_directory}
     environment:
       - HUGGING_FACE_HUB_TOKEN={hf_token}
       - HF_HOME={hf_directory}
-      - CUDA_VISIBLE_DEVICES='""'
     entrypoint: ["/bin/bash", "-c"]
     command: ["sleep infinity"]
     profiles:
