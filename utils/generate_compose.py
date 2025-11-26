ecf5a1e38fe31499c95c7ecdfa899d5f6bfd826c
Dmitry Trifonov <slonegg@gmail.com>
2025-11-25 19:24:23 -0800

    set visible devices to '""'

--- utils/generate_compose.py
+++ utils/generate_compose.py
@@ -135,7 +135,7 @@ def generate_benchmark_service(hf_directory: str, hf_token: str) -> str:
     environment:
       - HUGGING_FACE_HUB_TOKEN={hf_token}
       - HF_HOME={hf_directory}
-      - CUDA_VISIBLE_DEVICES="0"
+      - CUDA_VISIBLE_DEVICES='""'
     entrypoint: ["/bin/bash", "-c"]
     command: ["sleep infinity"]
     profiles:
