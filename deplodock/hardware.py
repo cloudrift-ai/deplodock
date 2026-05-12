59aa73497199e627c07da73fa510ef27c45fa786
Natalia Trifonova <koshmanova.n@gmail.com>
2026-05-12 12:52:52 -0700

    Add new h200 instance type

--- deplodock/hardware.py
+++ deplodock/hardware.py
@@ -37,6 +37,7 @@ GPU_INSTANCE_TYPES = {
         ("gcp", "a3-highgpu"),
     ],
     "NVIDIA H200 141GB": [
+        ("cloudrift", "h200-8-generic"),
         ("cloudrift", "h200-24-200-1000-generic"),
         ("gcp", "a3-ultragpu"),
     ],
