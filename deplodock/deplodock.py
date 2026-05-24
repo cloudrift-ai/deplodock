61b0da2c45705a0faea33a554705b38bf5d33e13
Dmitry Trifonov <slonegg@gmail.com>
2026-05-24 12:19:28 -0700

    add deplodock knobs command

--- deplodock/deplodock.py
+++ deplodock/deplodock.py
@@ -9,6 +9,7 @@ from deplodock.commands.deploy.cloud import register_cloud_target
 from deplodock.commands.deploy.local import register_local_target
 from deplodock.commands.deploy.ssh import register_ssh_target
 from deplodock.commands.inspect_graph import register_inspect_command
+from deplodock.commands.knobs import register_knobs_command
 from deplodock.commands.pull import register_pull_command
 from deplodock.commands.run import register_run_command
 from deplodock.commands.teardown import register_teardown_command
@@ -42,6 +43,7 @@ def main():
     register_tune_command(subparsers)
     register_run_command(subparsers)
     register_inspect_command(subparsers)
+    register_knobs_command(subparsers)
 
     args = parser.parse_args()
     setup_cli_logging()
